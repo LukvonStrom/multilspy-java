f1246159076c5bdce9982daaa3cf0956a87cf33c
Lukas Fruntke <lukas@fruntke.tech>
2024-07-24 14:50:03 +0100

    remove unnescessary print statement

--- multilspy-java-docker/init.sh
+++ multilspy-java-docker/init.sh
@@ -3,8 +3,5 @@
 # Activate the virtual environment
 source .env/bin/activate
 
-# Print the full command
-echo "Running command: timeout -k 10s 300s python cli_entrypoint.py $@"
-
 # Run the Python script with all passed arguments
 timeout -k 10s 300s python cli_entrypoint.py "$@"
